d9c1007970c8a74034e932726465104d8b1135dc
Saifa Meethong <smeethon@z3t5c1.42bangkok.com>
2022-08-20 23:15:03 +0700

    why am i here ?

--- count_vowel.py
+++ count_vowel.py
@@ -2,7 +2,7 @@ def get_count(sentence):
     y = 0
     x = 0
     while x < len(sentence):
-        if (sentence[x] == 'a' or sentence[x] == 'u' or sentence[x] == 'o' or entence[x] == 'e' or sentence[x] == 'i'):
+        if (sentence[x] == 'a' or sentence[x] == 'u' or sentence[x] == 'o' or sentence[x] == 'e' or sentence[x] == 'i'):
             y = y + 1
         x = x + 1
         return y
