cc5f5a6a4da4afbd06c9a55e179f1f0582b4e239
Saifa Meethong <smeethon@z3t5c1.42bangkok.com>
2022-08-20 23:05:03 +0700

    42 wf

---
+++ count_vowel.py
@@ -0,0 +1,9 @@
+def get_count(sentence):
+    y = 0
+    x = 0
+    while x < len(sentence):
+        if (sentence[x] == 'a' or sentence[x] == 'u' or sentence[x] == 'o' or entence[x] == 'e' or sentence[x] == 'i'):
+            y = y + 1
+        x = x + 1
+        return y
+        pass
