3c29d97a568a2cbe95a8023d028458dc80917c44
Saifa Meethong <smeethon@z3t5c1.42bangkok.com>
2022-08-25 01:19:19 +0700

    not finish

---
+++ hashtag.py
@@ -0,0 +1,19 @@
+def generate_hashtag(s):
+    if not s or len(s) > 140:
+        return False
+    x = s.split(" ")
+    y = len(x)
+    y2 = 0
+    x2 =[]
+    z = ""
+    i = 0
+    while y > y2:
+        x2.append(x[y2])
+        y2 = y2 + 1
+
+    while i < len(x2) :
+        z = z + x2[i]
+        i = i + 1
+    return("#" + z)
+print(generate_hashtag("hello there"))
+print(generate_hashtag(""))
