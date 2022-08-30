3014ea0f82bb23a2766ece721a4a9a4dbff8b567
Saifa Meethong <saifa36622@gmail.com>
2022-08-30 23:21:21 +0700

    loop

--- Loop2Right.py
+++ Loop2Right.py
@@ -5,7 +5,7 @@ def Loop2Right(n):
   y = 0
   for i in range(0,n):
     st.append("0")
-  while n > 0 or y < n:
+  while n > 0:
     st2 = list(st)
     st2[y] = n-1
     total += "".join(map(str,st2))
