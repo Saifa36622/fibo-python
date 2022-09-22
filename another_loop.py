1b500f1b5a1e3c7b3f1a08d259c8125ec3251bf4
Saifa Meethong <saifa36622@gmail.com>
2022-09-22 22:17:10 +0700

    yolo

---
+++ another_loop.py
@@ -0,0 +1,21 @@
+def f(x):
+    z = []
+    z.append(1)
+    loop = 2
+    total = ""
+    plus = 3
+    mid_total = 1
+    for y in range (1,x):
+        for i in range (y):
+            z.append(loop)
+            loop -= 2
+        loop += 2
+        mid_total = mid_total + plus
+        plus += 2
+        z.append(mid_total)
+        for o in range (y):
+            z.append(loop)
+            loop += 2
+
+    total += "".join(map(str,z))
+    return total
